98019064dec656f2720a2844ae296959c1b11bd5
Sanmugapriya <sanmugapriya.b@egrovesystems.com>
2016-05-13 20:36:06 +0530

    Github update completed

--- main.py
+++ main.py
@@ -38,11 +38,15 @@ def upload_files_to_git(file, name_dicts):
         settings.GIT_BRANCH = 'abap'
     elif name_dicts.get('TABLE', False):
         settings.GIT_BRANCH = 'table'
+
+    if name_dicts.get('EMAIL', False):
+        settings.GIT_EMAIL = name_dicts.get('EMAIL')
+    print settings.GIT_EMAIL
     params = dict()
 
     with open(file.name) as infile:
         content_encoded = base64.b64encode(infile.read())
-    params['message'] = file.name + " updated"
+    params['message'] = file.name + " created"
     params['content'] = content_encoded
     params['branch'] = settings.GIT_BRANCH
     params['path'] = file.name
@@ -52,33 +56,42 @@ def upload_files_to_git(file, name_dicts):
     if request_status.status_code == 201:
         print file.name + ' is created'
     elif request_status.status_code == 422:
-        print file.name + ' already created'
+        new_params = dict()
+        new_params['ref'] = settings.GIT_BRANCH
+        new_params['path'] = file.name
+        get_file = requests.get(url, auth=(settings.GIT_USERNAME, settings.GIT_PASSWORD), params=new_params).json()
+        params['sha'] = get_file['sha']
+        params['message'] = file.name + " updated"
+        request_update_status = requests.put(url, auth=(settings.GIT_USERNAME, settings.GIT_PASSWORD), data=json.dumps(params))
+        print file.name + ' updated'
     return True
 
 
 def file_reader(filename):
     search_for_start = True
-    search_for_finish = False
+    #search_for_finish = False
     with open(filename) as infile:
         for each_line in infile:
             if search_for_start:
                 if each_line.startswith('START:') or each_line.startswith('START TABLE:'):
                     name_dicts = get_names(each_line)
                     search_for_start = False
-                    search_for_finish = True
+                    #search_for_finish = True
                     file_name = name_dicts.get('START') or name_dicts.get('TABLE')
                     #name_dicts['START'] = check_for_dup_file(name_dicts['START'])
                     f_obj = open(file_name+'.txt','w')
 
-            if search_for_finish:
-                f_obj.writelines(each_line)
+            else:
+
                 if each_line.startswith('FINISH:') or each_line.startswith('FINISH TABLE:'):
                     search_for_start = True
-                    search_for_finish = False
+                    #search_for_finish = False
                     msg = name_dicts.get('START') or name_dicts.get('TABLE')
                     logging.info(msg +':: write-completed')
                     f_obj.close()
                     upload_files_to_git(f_obj, name_dicts)
+                else:
+                    f_obj.writelines(each_line)
 
 
 def main():
